ba09ca53fbc554d48c3901cc973ffb6f99d302fe
codexico <codexico@gmail.com>
2010-11-16 16:58:25 -0200

    thanks no footer

--- view/partial/footer.php
+++ view/partial/footer.php
@@ -1,8 +1,14 @@
 <div id="footer">
-    <a href="http://api.whatthetrend.com/" id="databywtt">
-    <?php if (PRODUCTION) ://para testar offline ?>
+    <span id="footer-left">
+    &nbsp;Thanks to <a href="http://jquery.com/">jQuery</a> plugins:
+        <a href="http://razorjack.net/quicksand/">Quicksand</a>,
+        <a href="http://code.drewwilson.com/entry/tiptip-jquery-plugin">TipTip</a>,
+        <a href="http://codexico.com.br/blog/jquery-lancelot-plugin">Lancelot</a>
+    <a href="http://api.whatthetrend.com/">
+    <?php //if (PRODUCTION) ://para testar offline ?>
         <img src="http://api.whatthetrend.com/images/wtt_api_badge_120.png" border="0" alt="data by What the Trend?">
-    <?php endif; ?>
+    <?php //endif; ?>
     </a>
-    <a href="http://codexico.com.br/blog" title="Codexico blog" id="codexicoblog">codexico</a>
+        </span>
+        <span id="codexicoblog">by <a href="http://codexico.com.br/blog" title="Codexico blog" >codexico</a></span>
 </div>
