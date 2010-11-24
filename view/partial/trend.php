1cb77f0f1c49882326202f03d48eecd3b491850f
codexico <codexico@gmail.com>
2010-11-24 16:29:31 -0200

    opcao 'Add a definition' e favicon melhorzinho

--- view/partial/trend.php
+++ view/partial/trend.php
@@ -1,12 +1,18 @@
 <span class="name">
-    <a href="<?= $trend->url ?>" class="trendname"
+    <a href="<?= $trend->url ?>" class="trendname" title="
     <?php if (isset($trend->description->text)) : ?>
-           title="<?= preg_replace('/"/', "'", $trend->description->text); ?>"
-       <? endif; ?>>
+           <?= preg_replace('/"/', "'", $trend->description->text); ?>
+       <?php else : ?>
+            Not explained yet.<br />
+            <a target='_blank' href='http://whatthetrend.com/trend/<?= $trend->name; ?>'>
+                Add a defintion!
+            </a>
+       <?php endif; ?>">
            <?= $trend->name ?>
-    </a>
-    <a href="<?= $trend->url ?>" class="twittericon lancelot"
-       title="twitter search: <?= $trend->name ?>"></a>
-    <a href="http://www.google.com/search?q=<?= preg_replace('/#/', '', $trend->name) ?>" class="googleicon lancelot"
-       title="google search: <?= $trend->name ?>"></a>
+     </a>
+     <a href="<?= $trend->url ?>" class="twittericon lancelot"
+        title="twitter search: <?= $trend->name ?>"></a>
+     <a href="http://www.google.com/search?q=<?= preg_replace('/#/', '', $trend->name) ?>"
+        class="googleicon lancelot"
+        title="google search: <?= $trend->name ?>"></a>
 </span>
